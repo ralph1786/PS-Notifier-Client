71d0c4ed27c1c2331f3b9f39590b4e24607dbe12
ralph1786 <ralph1786@gmail.com>
2019-08-15 12:35:22 -0400

    created notification icon

---
+++ src/components/NotificationIcon.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./NotificationIcon.css";
+
+function NotificationIcon(props) {
+  return (
+    <div>
+      <button className="notification_icon">2</button>
+    </div>
+  );
+}
+
+export default NotificationIcon;
