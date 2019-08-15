161556e794fa5eac4029782363270bd2331f90d0
ralph1786 <ralph1786@gmail.com>
2019-08-15 12:23:09 -0400

    created Avatar component

---
+++ src/components/Avatar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Avatar.css";
+
+function Avatar() {
+  return (
+    <div>
+      <button className="user_avatar">RC</button>
+    </div>
+  );
+}
+
+export default Avatar;
