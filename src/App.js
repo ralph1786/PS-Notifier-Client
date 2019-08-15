be5c607e0a202e6ef1763d6d01e0f9978d520e06
ralph1786 <ralph1786@gmail.com>
2019-08-15 13:16:49 -0400

    added Modal Container

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Fragment } from "react";
 import "./App.css";
 import Avatar from "./components/Avatar";
 import NotificationIcon from "./components/NotificationIcon";
+import ModalContainer from "./containers/ModalContainer";
 
 function App() {
   return (
     <Fragment>
       <Avatar />
       <NotificationIcon />
+      <ModalContainer />
     </Fragment>
   );
 }
