be676ce92229ff3b32e255cbab305ab7374a1681
ralph1786 <ralph1786@gmail.com>
2019-08-15 13:18:20 -0400

    connected component to redux, can dispatch actions

--- src/components/NotificationIcon.js
+++ src/components/NotificationIcon.js
@@ -1,12 +1,27 @@
 import React from "react";
 import "./NotificationIcon.css";
+import { connect } from "react-redux";
+import { openModal } from "../reduxStore/actions/actions";
 
 function NotificationIcon(props) {
   return (
     <div>
-      <button className="notification_icon">2</button>
+      <button onClick={props.openModal} className="notification_icon">
+        {props.content === null ? 0 : 2}
+      </button>
     </div>
   );
 }
 
-export default NotificationIcon;
+const mapStateToProps = state => ({
+  content: state.content
+});
+
+const mapDispatchToProps = dispatch => ({
+  openModal: () => dispatch(openModal())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(NotificationIcon);
