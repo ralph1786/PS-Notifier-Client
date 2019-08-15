9d0736bccd9a006c0c5ebff1b936ceeeccaecb40
ralph1786 <ralph1786@gmail.com>
2019-08-15 14:32:06 -0400

    changed app to class component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React, { Fragment } from "react";
+import React, { Fragment, Component } from "react";
 import "./App.css";
+import { connect } from "react-redux";
 import Avatar from "./components/Avatar";
 import NotificationIcon from "./components/NotificationIcon";
 import ModalContainer from "./containers/ModalContainer";
+import { fetchContent } from "./reduxStore/actions/actions";
 
-function App() {
-  return (
-    <Fragment>
-      <Avatar />
-      <NotificationIcon />
-      <ModalContainer />
-    </Fragment>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchContent();
+  }
+  render() {
+    return (
+      <Fragment>
+        <Avatar />
+        <NotificationIcon />
+        <ModalContainer />
+      </Fragment>
+    );
+  }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  fetchContent: () => dispatch(fetchContent())
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(App);
