243ff63654d1fb345c7c35602a38027fdef472cc
ralph1786 <ralph1786@gmail.com>
2019-08-15 12:34:45 -0400

    added notification icon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from "react";
 import "./App.css";
 import Avatar from "./components/Avatar";
+import NotificationIcon from "./components/NotificationIcon";
 
 function App() {
   return (
     <Fragment>
       <Avatar />
+      <NotificationIcon />
     </Fragment>
   );
 }
