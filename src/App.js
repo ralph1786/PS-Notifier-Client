fd51c28f744ae502bbbd4c2bc878f79ad698c380
ralph1786 <ralph1786@gmail.com>
2019-08-15 12:22:25 -0400

    added Avatar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./App.css";
+import Avatar from "./components/Avatar";
 
 function App() {
   return (
-    <div className="App">
-      <h2>Hello User</h2>
-    </div>
+    <Fragment>
+      <Avatar />
+    </Fragment>
   );
 }
 
