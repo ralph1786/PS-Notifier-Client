730da807e124bbaaf9c301c12a961f6454e32aad
ralph1786 <ralph1786@gmail.com>
2019-08-15 12:09:35 -0400

    removed unnecessary content

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h2>Hello User</h2>
-      </header>
+      <h2>Hello User</h2>
     </div>
   );
 }
