0e1a200183ea617143b4599e250f5ba8286199b0
ralph1786 <ralph1786@gmail.com>
2019-08-15 14:32:40 -0400

    added fetchContent to retrieve info

--- src/reduxStore/actions/actions.js
+++ src/reduxStore/actions/actions.js
@@ -1,3 +1,10 @@
+import axios from "axios";
+
+export const setContent = data => ({
+  type: "SET_CONTENT",
+  payload: data
+});
+
 export const openModal = () => ({
   type: "OPEN_MODAL"
 });
@@ -5,3 +12,16 @@ export const openModal = () => ({
 export const closeModal = () => ({
   type: "CLOSE_MODAL"
 });
+
+export const fetchContent = () => {
+  return dispatch => {
+    return axios
+      .get("http://localhost:5000")
+      .then(res => {
+        console.log(res.data.info);
+        const markDownInfo = res.data.info;
+        dispatch(setContent(markDownInfo));
+      })
+      .catch(err => console.log(err));
+  };
+};
